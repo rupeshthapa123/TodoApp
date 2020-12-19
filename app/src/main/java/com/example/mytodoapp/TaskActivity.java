48eb9b63216f5358e25b346d0281b0538675b82b
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2020-12-19 20:43:01 +0545

    Database File Updated

--- app/src/main/java/com/example/mytodoapp/TaskActivity.java
+++ app/src/main/java/com/example/mytodoapp/TaskActivity.java
@@ -1,6 +1,9 @@
 package com.example.mytodoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,12 +11,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.mytodoapp.data.Task;
 import com.example.mytodoapp.data.TaskRepository;
 
-public class TaskActivity extends AppCompatActivity {
+import java.util.List;
 
+public class TaskActivity extends AppCompatActivity {
+/*
     private TextView title;
     private TextView description;
     private CheckBox isComplete;
@@ -21,12 +27,27 @@ public class TaskActivity extends AppCompatActivity {
     Task task;
     private Button detail;
     TaskRepository repository = TaskRepository.getInstance();
+*/
+    private TaskViewModel taskViewModel;
+    private Object ViewModelProviders;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+        TaskViewModel taskViewModel = new ViewModelProvider(this).get(TaskViewModel.class);
+        taskViewModel.getAlltasks().observe(this, new Observer<List<Task>>() {
+            @Override
+            public void onChanged(List<Task> tasks) {
+                Toast.makeText(TaskActivity.this,"OnChange",Toast.LENGTH_SHORT).show();
+            }
+        });
 
+
+
+
+         /*
         title = findViewById(R.id.title_id);
         description = findViewById(R.id.desc_id);
         isComplete = findViewById((R.id.iscom_id));
@@ -49,12 +70,12 @@ public class TaskActivity extends AppCompatActivity {
                 Intent intent = new Intent();
                 startActivity(intent);
             }
-        });
+        });*/
     }
-
+/*
     private void updateUI(Task task) {
         title.setText(task.getTitle());
         description.setText(task.getDescription());
         isComplete.setChecked(task.isComplete());
-    }
+    }*/
 }
