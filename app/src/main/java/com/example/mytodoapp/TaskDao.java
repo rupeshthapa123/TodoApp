f9b46a860b2ee75e067b3c93c3ba1473b6bd7f77
rupeshthapa123 <rupesh.thapa2050@gmail.com>
2020-12-19 16:06:02 +0545

    Dao File Created

---
+++ app/src/main/java/com/example/mytodoapp/TaskDao.java
@@ -0,0 +1,23 @@
+package com.example.mytodoapp;
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import com.example.mytodoapp.data.Task;
+
+import java.util.List;
+
+@Dao
+public interface TaskDao {
+    @Insert
+    void insert(Task task);
+    @Update
+    void update(Task task);
+    @Delete
+    void delete(Task task);
+    @Query("SELECT * FROM task_table ORDER BY priority DESC")
+    LiveData<List<Task>> getAllTasks();
+}
